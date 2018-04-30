eb9d50e3bcecee3cc6a4593336904e5f1933cedc
Renan L. Bianchini <renan@waiodev.com.br>
2018-04-30 15:31:17 -0300

    Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../Recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-    recipes = [];
+    recipes: Recipe[] = [
+        new Recipe('a Test Recipe', 'this is simply a test','https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+    ];
 
   constructor() { }
 
